f5a9f20e98b1e204f63c61b4a6997152c416acb9
truongdang <quangtruonglf@gmail.com>
2025-04-09 00:34:17 +0700

    imported files needed for sims

---
+++ launch/robot_state_publisher.launch.py
@@ -0,0 +1,36 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+        package_name = 'myslam'
+
+        # Parse robot description from xacro
+        file_subpath = 'description/robot.urdf.xacro'
+        xacro_file = os.path.join(
+                get_package_share_directory(package_name),
+                file_subpath
+        )
+        robot_description = xacro.process_file(xacro_file).toxml()
+
+        use_sim_time = LaunchConfiguration('use_sim_time')
+        params = {'use_sim_time': use_sim_time, 'robot_description': robot_description}
+        node_robot_state_publisher = Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name="robot_state_publisher",
+                output='screen',
+                parameters=[params],
+        )
+
+        return LaunchDescription([
+                DeclareLaunchArgument(
+                        'use_sim_time',
+                        default_value='true',
+                        description='Use sim time if true'),
+                node_robot_state_publisher
+        ])
---
+++ launch/sim.launch.py
@@ -0,0 +1,40 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+        package_name = 'myslam' 
+ 
+        gazebo = IncludeLaunchDescription(
+                        PythonLaunchDescriptionSource([os.path.join(
+                                get_package_share_directory(package_name), 'launch', 'gz.launch.py')])
+        )
+        
+        rviz = Node(
+                package='rviz2',
+                executable='rviz2',
+                arguments=['-d', os.path.join(get_package_share_directory(package_name), 'config', 'sim_config.rviz')],
+                output='screen'
+        )
+
+        teleop_keyboard = Node(
+                package='teleop_twist_keyboard',
+                executable='teleop_twist_keyboard',
+                output='screen',
+                prefix = 'xterm -e',
+                parameters=[{'stamped': True}],
+                remappings=[('/cmd_vel', '/diff_controller/cmd_vel')]
+                        
+        )
+
+        return LaunchDescription([
+                gazebo,
+                rviz,
+                teleop_keyboard
+        ])
